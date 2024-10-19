e1ce1ac83f4ca2cd7d83dbc79e8eb95c417eb461
phamhuan3126 <phamhuan.net@gmail.com>
2024-10-19 16:56:04 +0700

    full commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import { Button, Box, Container, Space } from '@mantine/core';
+
 export default function HomePage() {
-  return <div>Home page</div>;
+  return (
+    <Container>
+      <Space h="md" />
+      <Box>
+          <Button variant="filled">Button ab</Button>
+      </Box>
+    </Container>
+  ); 
 }
